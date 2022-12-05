edc938a130711138aa4f270f8377fe011d0e2a57
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2022-12-05 22:30:24 +0700

    fix type errors
    as per this guide:
    https://docs.slatejs.org/concepts/12-typescript#migrating-from-0.47.x

--- apps/hello-nextjs/index.d.ts
+++ apps/hello-nextjs/index.d.ts
@@ -1,4 +1,4 @@
-// /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { BaseEditor } from 'slate';
 import { ReactEditor } from 'slate-react';
 import { HistoryEditor } from 'slate-history';
