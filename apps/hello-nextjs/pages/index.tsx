b08b1f6f263173391497c4a596b2aa0ad9262b2c
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2022-12-05 10:05:21 +0700

    implement the RichTextEditor
    built based on the following examples:
    - https://github.com/ianstormtaylor/slate/blob/main/site/components.tsx
    - https://www.slatejs.org/examples/richtext

--- apps/hello-nextjs/pages/index.tsx
+++ apps/hello-nextjs/pages/index.tsx
@@ -1,10 +1,13 @@
 import styles from './index.module.css';
+import RichTextEditor from '../src/components/RichTextEditor';
 
 export function Index() {
   return (
     <div className={styles.page}>
       <div className="wrapper">
-        <div className="container"></div>
+        <div className="container">
+          <RichTextEditor />
+        </div>
       </div>
     </div>
   );
